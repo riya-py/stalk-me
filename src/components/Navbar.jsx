78ec086420c443b6be3904e6fc726747333b65e4
riya-py <lowkeywhocares@gmail.com>
2026-08-06 18:34:52 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { navLinks, navIcons } from "#constants/index.js";
+import dayjs from "dayjs";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-bold">My Portfolio</p>
+
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.id}>
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon-hover" />
+            </li>
+          ))}
+        </ul>
+        <time>
+            {dayjs().format("ddd MMM D, h:mm A")}
+        </time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import {resolve, dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+// ./../components/something
+// @components/component
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'src/components'),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'src/constants'),
+      '#store': resolve(dirname(fileURLToPath(import.meta.url)), 'src/store'),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'src/hoc'),
+      '#assets': resolve(dirname(fileURLToPath(import.meta.url)), 'src/assets'),
+    }
+  }
+})
