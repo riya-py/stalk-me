a2d6408298b682a414a63e406b02afab7ee95c60
riya-py <lowkeywhocares@gmail.com>
2026-08-13 15:35:52 +0530

    finder finalllyy doneee

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -15,6 +15,8 @@ const Finder = () => {
     if(item.kind==='folder') return setActiveLocation(item)
     if(['fig','url'].includes(item.fileType) && item.href)
         return window.open(item.href, "_blank")
+
+    openWindow(`${item.fileType}${item.kind}`, item)
   }
 
   const renderList = (items) =>
